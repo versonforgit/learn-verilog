899fb8cafa92fbfd15b07c4a8cacce87eda570ac
versonforgit <201505100133@mail.sdu.edu.cn>
2022-05-10 22:38:46 +0800

    conuter10

---
+++ counter10/counter10_tb.v
@@ -0,0 +1,46 @@
+`timescale 1 ns/1 ns
+module counter10_tb ;
+
+reg 		rstn;
+reg 		clk;
+wire [3:0]	cnt;
+wire		cout;
+
+counter10 C10(
+	.rstn(rstn),
+	.clk(clk),
+	.cnt(cnt),
+	.cout(cout)
+	);
+	
+	
+initial
+begin
+	clk = 1'b0;
+	rstn = 1'b0;
+	#11 ;
+	rstn = 1'b1;
+end
+
+always
+begin
+	#10 ;
+	clk = ~clk;
+end
+	
+/*iverilog */
+initial
+begin            
+    $dumpfile("wave.vcd");        //生成的vcd文件名称
+    $dumpvars(0, counter10_tb);     //tb模块名称
+end
+
+
+/*duration of the simulation*/
+
+initial
+#800 $finish;
+
+
+
+endmodule
